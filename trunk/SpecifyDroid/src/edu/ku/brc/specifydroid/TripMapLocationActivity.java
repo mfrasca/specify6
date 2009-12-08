d79185614a842b1f82aacfe14a3cd05b05bf8846
rods <rods@ce9bf4bf-376f-1848-a496-a1c9fe3b1a39>
2009-12-08 17:55:27 +0000

    Snapshot for ATBI Demo

--- trunk/SpecifyDroid/src/edu/ku/brc/specifydroid/TripMapLocationActivity.java
+++ trunk/SpecifyDroid/src/edu/ku/brc/specifydroid/TripMapLocationActivity.java
@@ -1,20 +1,15 @@
 package edu.ku.brc.specifydroid;
 
-import java.util.HashMap;
-
-import android.database.Cursor;
+import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Window;
 
-import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
-import com.google.android.maps.OverlayItem;
-import com.pocketjourney.view.MapLocationViewer;
+import com.pocketjourney.view.TransparentPanel;
 
 public class TripMapLocationActivity extends MapActivity {
 
-    private String tripId;
+    //private String tripId;
 
     /* (non-Javadoc)
      * @see com.google.android.maps.MapActivity#onCreate(android.os.Bundle)
@@ -29,65 +24,15 @@ public class TripMapLocationActivity extends MapActivity {
         
         setContentView(R.layout.maplocviewer);
         
-        tripId = getIntent().getStringExtra(TripListActivity.ID_EXTRA);
+        //tripId = getIntent().getStringExtra(TripListActivity.ID_EXTRA);
         
-        GeoPoint firstPnt = null;
+        Paint innerPaint = new Paint();
+        innerPaint.setARGB(0, 255, 255, 255); //gray
+        innerPaint.setAntiAlias(true);
 
-        String sql = String.format("SELECT tc._id, tc.Data, tc.TripRowIndex, td.Name FROM tripdatacell tc INNER JOIN tripdatadef td ON tc.TripDataDefID = td._id WHERE tc.TripID = %s ORDER BY tc.TripRowIndex, td.ColumnIndex", tripId);
-        Cursor  cursor = SpecifyActivity.getDatabase().rawQuery(sql, null);
-        if (cursor.moveToFirst())
-        {
-            HashMap<String, String> dataHash = new HashMap<String, String>();
-            
-            Integer rowIndex = null;
-            int dInx  = cursor.getColumnIndex("Data");
-            int rInx  = cursor.getColumnIndex("TripRowIndex");
-            int nInx  = cursor.getColumnIndex("Name");
-            do
-            {
-                int rowNum = cursor.getInt(rInx);
-                if (rowIndex == null)
-                {
-                    rowIndex = rowNum;
-                    
-                } 
-                    
-                if (rowIndex != rowNum)
-                {
-                    String name = dataHash.get("LocalityName");
-                    int    lat  = (int)(Double.parseDouble(dataHash.get("Latitude")) * 1000000.0);
-                    int    lon  = (int)(Double.parseDouble(dataHash.get("Longitude")) * 1000000.0);
-                    
-                    Log.d("debug", lat+ " = "+lon);
-                    GeoPoint    point       = new GeoPoint(lat,lon);
-                    OverlayItem overlayitem = new OverlayItem(point, name, name);
-                    //itemizedOverlay.addOverlay(overlayitem); 
-                    dataHash.clear();
-                    rowIndex = rowNum;
-                    
-                    if (firstPnt == null)
-                    {
-                        firstPnt = point;
-                    }
-                } 
-                
-                Log.d("debug", cursor.getString(nInx)+ " = "+cursor.getString(dInx));
-                
-                dataHash.put(cursor.getString(nInx), cursor.getString(dInx));
-                
-                
-            } while (cursor.moveToNext());
-            cursor.close();
-        }
-        
-        /*mapOverlays.add(itemizedOverlay);
-        
-        if (firstPnt != null)
-        {
-            MapController mc = mapView.getController();
-            mc.animateTo(firstPnt);
-            mc.setZoom(12); 
-        }*/
+        TransparentPanel transPanel = (TransparentPanel)findViewById(R.id.transparent_panel);
+        transPanel.setInnerPaint(innerPaint);
+        transPanel.setBorderPaint(innerPaint);
     }
 
     /* (non-Javadoc)
