8e0cb7fa4f78ab9e00d9cb1ef6a024980a82407c
idali0226 <idali0226@ce9bf4bf-376f-1848-a496-a1c9fe3b1a39>
2012-05-08 11:06:18 +0000

    1. add new method 2. change dependency in pom

--- SpecifyEnterprise/SpecifyEJB/specify-business-logic/src/main/java/se/nrm/specify/business/logic/specify/SpecifyLogicImpl.java
+++ SpecifyEnterprise/SpecifyEJB/specify-business-logic/src/main/java/se/nrm/specify/business/logic/specify/SpecifyLogicImpl.java
@@ -1,7 +1,11 @@
 package se.nrm.specify.business.logic.specify;
 
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless; 
+import se.nrm.specify.datamodel.Collectionobject; 
+import se.nrm.specify.datamodel.Dnasequence;
+import se.nrm.specify.datamodel.Taxon;
 import se.nrm.specify.specify.data.jpa.SpecifyDao;
 
 /**
@@ -10,14 +14,13 @@ import se.nrm.specify.specify.data.jpa.SpecifyDao;
  */
 @Stateless
 public class SpecifyLogicImpl implements SpecifyLogic {
-    
+     
     @EJB
     private SpecifyDao dao;
-    
+
     public SpecifyLogicImpl() {
-        
     }
-    
+
     public SpecifyLogicImpl(SpecifyDao dao) {
         this.dao = dao;
     }
@@ -25,6 +28,40 @@ public class SpecifyLogicImpl implements SpecifyLogic {
     public SpecifyDao getDao() {
         return dao;
     }
-    
-    
+
+    /**
+     * This method retrieves dna sequences from database, with dna sequences to 
+     * create a .fasta file for creating blast database.  A .fasta file is built 
+     * with ids (combined with collectionobjectid and taxon names) and corresponding 
+     * dna sequences.
+     * 
+     * @return String contains dna sequences
+     */
+    public String getDNASequenceList() {
+
+        StringBuilder sb = new StringBuilder();
+
+        List<Dnasequence> list = dao.getAll(Dnasequence.class);        // List of Dnasequence from database                       
+        for (Dnasequence sequence : list) {
+            Collectionobject collectionObj = sequence.getCollectionObjectID();
+            
+            String jpql = "SELECT d FROM Determination d WHERE d.collectionObjectID.collectionObjectID = " + 
+                                                        collectionObj.getCollectionObjectID() + " and d.isCurrent = true";
+            
+            Taxon taxon = (Taxon)dao.getEntityByJPQL(jpql);
+//            Taxon taxon = dao.getTaxonByCollectionobject(collectionObj);    // get taxon by collectionobject from database
+                
+            sb.append(">");
+            sb.append(collectionObj.getCollectionObjectID());
+            sb.append("::");
+            sb.append(taxon.getFullName());
+            sb.append("::");
+            sb.append(taxon.getCommonName());
+            sb.append("\n");
+            sb.append(sequence.getGeneSequence().replaceAll("[0-9 \t\n\r]", ""));
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
 }
