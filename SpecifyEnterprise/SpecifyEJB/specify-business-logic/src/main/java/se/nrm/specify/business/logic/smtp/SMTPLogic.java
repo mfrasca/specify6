bee8a4d5f963ebc37d1a8775cb1e11e5cdad0ff7
idali0226 <idali0226@ce9bf4bf-376f-1848-a496-a1c9fe3b1a39>
2012-06-06 10:35:45 +0000

    Change entity fields name match to field name defined in specify_datamodel.xml

--- SpecifyEnterprise/SpecifyEJB/specify-business-logic/src/main/java/se/nrm/specify/business/logic/smtp/SMTPLogic.java
+++ SpecifyEnterprise/SpecifyEJB/specify-business-logic/src/main/java/se/nrm/specify/business/logic/smtp/SMTPLogic.java
@@ -1,6 +1,8 @@
 package se.nrm.specify.business.logic.smtp;
 
+import java.util.List;
 import se.nrm.specify.datamodel.DataWrapper; 
+import se.nrm.specify.datamodel.Locality;
 
 /**
  *
@@ -8,5 +10,8 @@ import se.nrm.specify.datamodel.DataWrapper;
  */ 
 public interface SMTPLogic {
     
+    public DataWrapper getDeterminationsData(String taxonNode, String highestChildNode, String CollectionCode);
+    public String getTaxonAndParents(String taxonname);
+    public List<Locality> getLocalityByCollectionCode(String collectionCode);
     public void saveSMTPBatchData(DataWrapper wrapper, String userid);
 }
